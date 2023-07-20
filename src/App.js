3d43e5b1417f296c3d0f265ffb6b27285ee83e77
16684814105 <402150786@qq.com>
2023-07-21 07:02:35 +0800

    dynamic rendering lists

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
 import Expense from "./components/Expense/Expense";
 import InfoGather from "./components/InfoGather/InfoGather";
 import { useState } from "react";
+const _expenses = [
+  {
+    id: "e1",
+    title: "演唱会",
+    amount: 388,
+    date: new Date(2022, 7, 12),
+  },
+  {
+    id: "e2",
+    title: "LiveShow",
+    amount: 288,
+    date: new Date(2023, 4, 30),
+  },
+  {
+    id: "e3",
+    title: "展览",
+    amount: 120,
+    date: new Date(2023, 5, 30),
+  },
+  {
+    id: "e4",
+    title: "电影",
+    amount: 60,
+    date: new Date(2023, 5, 20),
+  },
+];
 
 function App() {
-  const _expenses = [
-    {
-      id: "e1",
-      title: "演唱会",
-      amount: 388,
-      date: new Date(2022, 7, 12),
-    },
-    {
-      id: "e2",
-      title: "LiveShow",
-      amount: 288,
-      date: new Date(2023, 4, 30),
-    },
-    {
-      id: "e3",
-      title: "展览",
-      amount: 120,
-      date: new Date(2023, 5, 30),
-    },
-    {
-      id: "e4",
-      title: "电影",
-      amount: 60,
-      date: new Date(2023, 5, 20),
-    },
-  ];
   const [expenses, setExpenses] = useState(_expenses);
   const getExpenseDate = (data) => {
-    setExpenses((prevalue) => {
-      console.log(prevalue);
-      return [data, ...prevalue];
-    });
+    setExpenses((prevalue) => [data, ...prevalue]);
   };
   return (
     <div>
--- src/components/ExpensesFilter/ExpensesFilter.js
+++ src/components/ExpensesFilter/ExpensesFilter.js
@@ -1,14 +1,15 @@
 import "./ExpensesFilter.css";
 
-const ExpensesFilter = ({ onGetYear }) => {
+const ExpensesFilter = ({ selected, onSetFilterYear }) => {
   const filterChangeHandler = (selectedYear) => {
-    onGetYear(selectedYear.target.selectedOptions[0].value);
+    onSetFilterYear(selectedYear.target.selectedOptions[0].value);
   };
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
-        <label>年份</label>
-        <select onChange={filterChangeHandler}>
+        <label>目前为{selected}花费</label>
+        <select value={selected} onChange={filterChangeHandler}>
+          <option value="all">全部</option>
           <option value="2024">2024</option>
           <option value="2023">2023</option>
           <option value="2022">2022</option>
